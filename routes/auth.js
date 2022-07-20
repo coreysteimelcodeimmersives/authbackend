fee9fb9481da5f235ed025076ff583962da06a79
Corey Steimel <coreysteimel@Coreys-MacBook-Pro-Work.local>
2022-07-20 13:49:34 -0600

    wed done

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,12 @@
+const dotenv = require("dotenv");
+const jwt = require("jsonwebtoken");
 var express = require("express");
 var router = express.Router();
 const bcrypt = require("bcryptjs");
 const { uuid } = require("uuidv4");
 const { blogsDB } = require("../mongo");
+
+dotenv.config();
 
 const createUser = async (username, passwordHash) => {
   try {
@@ -36,14 +40,46 @@ router.post("/register-user", async (req, res) => {
 
 router.post("/login-user", async (req, res) => {
   try {
+    const username = req.body.username;
+    const password = req.body.password;
     const collection = await blogsDB().collection("users");
     const user = await collection.findOne({
-      username: req.body.username,
+      username: username,
     });
-    const match = await bcrypt.compare(req.body.password, user.password);
-    res.json({ success: match }).status(200);
+    const match = await bcrypt.compare(password, user.password);
+    if (match) {
+      const jwtSecretKey = process.env.JWT_SECRET_KEY;
+      const data = {
+        time: new Date(),
+        userId: user.uid,
+      };
+      const token = jwt.sign(data, jwtSecretKey);
+      res.json({ success: match, token: token }).status(200);
+      return;
+    }
+    res.json({ success: match }).status(204);
   } catch (error) {
     res.json({ success: error }).status(500);
+  }
+});
+
+router.get("/auth/validate-token", (req, res) => {
+  const tokenHeaderKey = process.env.TOKEN_HEADER_KEY;
+  const jwtSecretKey = process.env.JWT_SECRET_KEY;
+
+  try {
+    const token = req.header(tokenHeaderKey);
+
+    const verified = jwt.verify(token, jwtSecretKey);
+    if (verified) {
+      return res.json({ success: true });
+    } else {
+      // Access Denied
+      throw Error("Access Denied");
+    }
+  } catch (error) {
+    // Access Denied
+    return res.status(401).json({ success: true, message: String(error) });
   }
 });
 
